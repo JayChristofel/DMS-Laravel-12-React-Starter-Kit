bfab88c278778e63fa071a9c422245406ce3fa4a
JayChristofel <jaymandagi@yahoo.com>
2025-06-18 21:23:43 +0700

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             AdminUserSeeder::class,
             DummyDocumentSeeder::class,
-            NotificationEmailSeeder::class,
         ]);
         
         User::factory(10)->create([
